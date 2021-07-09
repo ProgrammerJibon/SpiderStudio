86729638270bcfdb12eb20c47b6ff98c41834970
𝔽𝕒𝕙𝕚𝕞𝕒 𝕄𝕒𝕪𝕟𝕒 <programmerjibon@gmail.com>
2021-07-10 03:42:51 +0600

    10/07/2021 03:42:36AM ProgrammerJibon

--- app/src/main/java/app/jibon/spider/MainActivity.java
+++ app/src/main/java/app/jibon/spider/MainActivity.java
@@ -1,7 +1,11 @@
 package app.jibon.spider;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -15,10 +19,13 @@ import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.lang.reflect.Parameter;
 import java.util.Timer;
@@ -27,17 +34,36 @@ import java.util.TimerTask;
 import static android.content.Context.MODE_WORLD_READABLE;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout nav_drawer;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button MainActivitySetMode = findViewById(R.id.nav_night_mode);
-        MainActivitySetMode.setOnClickListener(view->{
-            if (new Settings(getApplicationContext()).VisualModeSettings()){
-                new CustomToast(getApplicationContext(), "Visual Mode Changed");
-            }else{
-                new CustomToast(getApplicationContext(), "Something went wrong");
+        nav_drawer = findViewById(R.id.MainActivity);
+        navigationView = findViewById(R.id.nav_drawer);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, nav_drawer, toolbar, 0,0);
+        nav_drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_night_mode){
+                if (new Settings(getApplicationContext()).VisualModeSettings()){
+                    new CustomToast(getApplicationContext(), "Visual Mode Changed");
+                }else{
+                    new CustomToast(getApplicationContext(), "Something went wrong");
+                }
             }
+            if (id == R.id.nav_exit){
+                finish();
+            }
+            navigationView.clearFocus();
+            nav_drawer.closeDrawers();
+            return false;
         });
     }
     public class CustomToast {
