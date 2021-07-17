6aa6f1b03863e26e705743ac929697a7106dfe7a
𝔽𝕒𝕙𝕚𝕞𝕒 𝕄𝕒𝕪𝕟𝕒 <programmerjibon@gmail.com>
2021-07-17 12:13:29 +0600

    0

--- app/src/main/java/app/jibon/spider/MainActivity.java
+++ app/src/main/java/app/jibon/spider/MainActivity.java
@@ -1,8 +1,14 @@
 package app.jibon.spider;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -12,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
             new CustomToast(this, "Welcome sir!", R.drawable.ic_baseline_notifications_none_24);
             new NavigationDrawerSettings(this, R.id.nav_drawer);
+            try {
+                URL urlx = new URL("https://i.pinimg.com/736x/91/75/1f/91751f67c7ee60fc7742ee2e13c657e4.jpg");
+                URLConnection connection = urlx.openConnection();
+                Bitmap bitmap =  BitmapFactory.decodeStream(connection.getInputStream());
+                ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
+            }catch (Exception e){
+                new CustomToast(this, e.toString(), R.drawable.ic_baseline_warning_24);
+            }
+
         }catch (Exception e){
             new CustomToast(this, e.toString(), R.drawable.ic_baseline_warning_24);
         }
